16a74843cdd514ec9e7061aaea517a6aa55420e1
Rachael <rachael92njoki@gmail.com>
2024-01-19 11:18:07 +0300

    page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', addGoal);
 
 
 // API base URL
-const apiUrl = 'https://rayken20.github.io/fitapi/db.json';
+const apiUrl = 'https://fitness-fgpb.onrender.com/exercises';
 
 // Function to handle form submit and add goal to the database. Prevent the page from being refreshed immediately and allow us to do something else instead.
 function addGoal(event) {
